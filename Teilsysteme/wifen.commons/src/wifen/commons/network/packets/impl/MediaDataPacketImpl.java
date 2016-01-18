c1394e1fad3eebb88d1e245716d68503099ed93a
Felix <neoizen@web.de>
2016-01-18 08:28:52 +0100

    Adding ServerMediaService and Provider still not completed yet :(

--- Teilsysteme/wifen.commons/src/wifen/commons/network/packets/impl/MediaDataPacketImpl.java
+++ Teilsysteme/wifen.commons/src/wifen/commons/network/packets/impl/MediaDataPacketImpl.java
@@ -7,15 +7,48 @@ public class MediaDataPacketImpl extends PacketImpl implements MediaDataPacket
 {
 	private static final long serialVersionUID = -8597096663167790919L;
 
+	private String sourceName;
+	private String dataBlock;
+	
+	public MediaDataPacketImpl(String sourceName, String dataBlock)
+	{
+		this.sourceName = sourceName;
+		this.dataBlock = dataBlock;
+	}
+	
 	@Override
 	public String getSourceName()
 	{
-		return null;
+		return sourceName;
 	}
 
 	@Override
-	public String getMessage()
+	public String getDataBlock()
 	{
-		return null;
+		return dataBlock;
+	}
+
+	@Override
+	public String getFileType()
+	{
+		return dataBlock.split("^.*[.]{1}*[:]{1}*$")[1];
+	}
+
+	@Override
+	public String getFileName()
+	{
+		return dataBlock.split("^.*[.]{1}*[:]{1}*$")[0];
+	}
+
+	@Override
+	public String getFileData()
+	{
+		return dataBlock.split("^.*[.]{1}*[:]{1}*$")[2];
+	}
+
+	@Override
+	public String toString()
+	{
+		return "{" +  getSourceName() + ": " + getDataBlock() + "}";
 	}
 }
