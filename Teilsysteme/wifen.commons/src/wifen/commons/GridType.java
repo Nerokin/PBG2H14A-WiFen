6ad208997b56181bb79532e51159b2f307476f98
pbg2h14ajo <pbg2h14ajo@pb.bib.de>
2016-01-20 10:36:24 +0100

    Spielfeld Raster auf deutsch umgestellt.

--- Teilsysteme/wifen.commons/src/wifen/commons/GridType.java
+++ Teilsysteme/wifen.commons/src/wifen/commons/GridType.java
@@ -8,7 +8,25 @@ package wifen.commons;
  *
  */
 public enum GridType {
-	NONE,
-	SQUARE,
-	HEX;
+	NONE{
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return "Keins";
+		}
+	},
+	SQUARE{
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return "Quadratisch";
+		}
+	},
+	HEX{
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return "Sechseckig";
+		}
+	};
 }
